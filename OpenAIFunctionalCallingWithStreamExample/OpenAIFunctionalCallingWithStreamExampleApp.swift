4d18f2a3d8a0aa2562fa47cd2f3232fbd533789a
ysak-y <yoshiaki.0614@gmail.com>
2023-06-24 16:32:03 +0900

    Initial Commit

---
+++ OpenAIFunctionalCallingWithStreamExample/OpenAIFunctionalCallingWithStreamExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIFunctionalCallingWithStreamExampleApp.swift
+//  OpenAIFunctionalCallingWithStreamExample
+//
+//  Created by Yoshiaki Yamada on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIFunctionalCallingWithStreamExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
